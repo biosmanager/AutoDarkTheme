1a9f3e63d8609495bc44b7cdcc27db135a0ac8b9
biosmanager <matthias.albrecht@uni-konstanz.de>
2020-10-11 13:22:05 +0200

    User preferences

--- Editor/AutoDarkTheme.cs
+++ Editor/AutoDarkTheme.cs
@@ -9,66 +9,89 @@ using AutoDarkTheme.Windows;
 
 namespace AutoDarkTheme
 {
-	[InitializeOnLoad]
-	public class AutoDarkTheme
-	{
+    [InitializeOnLoad]
+    public class AutoDarkTheme
+    {
 #if UNITY_EDITOR_WIN
-		private static RegistryMonitor registryMonitor;
+        private static RegistryMonitor registryMonitor;
 #endif
 
-		static AutoDarkTheme()
-		{
-#if UNITY_EDITOR_WIN
-			// Windows: Watch system theme changes in registry
-			registryMonitor = new RegistryMonitor(RegistryHive.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize");
-			registryMonitor.RegChanged += new EventHandler(OnRegChanged);
-			registryMonitor.Start();
-#endif
+        static AutoDarkTheme()
+        {
+            UserPreferences.PreferencesChanged += new EventHandler(OnSettingsChanged);
 
-			// Set correct theme on start
-			SetEditorThemeFromSystemTheme();
-		}
+            MonitorThemeChanges();
+        }
 
-		~AutoDarkTheme() {
-#if UNITY_EDITOR_WIN
-			registryMonitor.Stop();
-#endif
-		}
-
-		private static void Update()
-		{
-			SetEditorThemeFromSystemTheme();
-			// Remove from update loop, this should only be called once after the system theme changes
-			EditorApplication.update -= Update;
-		}
-
-		private static void OnRegChanged(object sender, EventArgs e)
-		{
-			// Add Update() method to update loop, calling SwitchSkinAndRepaintAllViews() alone doesn't work
-			EditorApplication.update += Update;
-		}
-
-		private static void SetEditorThemeFromSystemTheme()
+        ~AutoDarkTheme()
         {
 #if UNITY_EDITOR_WIN
-			var appsUseLightTheme = registryMonitor.ReadDword("AppsUseLightTheme");
-
-			if (appsUseLightTheme == 1 && EditorGUIUtility.isProSkin)
-			{
-				Debug.Log("Editor: Switching to light theme....");
-
-				EditorPrefs.SetInt("UserSkin", 0);
-				InternalEditorUtility.SwitchSkinAndRepaintAllViews();
-			}
-			else if (appsUseLightTheme == 0 && !EditorGUIUtility.isProSkin)
-			{
-				Debug.Log("Editor: Switching to dark theme...");
-				EditorPrefs.SetInt("UserSkin", 1);
-				InternalEditorUtility.SwitchSkinAndRepaintAllViews();
-
-			}
+            registryMonitor?.Stop();
 #endif
-		}
-	}
+        }
+
+
+        private static void Update()
+        {
+            SetEditorThemeFromSystemTheme();
+            // Remove from update loop, this should only be called once after the system theme changes
+            EditorApplication.update -= Update;
+        }
+
+        private static void OnSettingsChanged(object sender, EventArgs e)
+        {
+            Debug.Log(UserPreferences.IsEnabled);
+            MonitorThemeChanges();
+        }
+
+        private static void OnRegChanged(object sender, EventArgs e)
+        {
+            // Add Update() method to update loop, calling SwitchSkinAndRepaintAllViews() alone doesn't work
+            EditorApplication.update += Update;
+        }
+
+
+        private static void MonitorThemeChanges()
+        {
+#if UNITY_EDITOR_WIN
+            registryMonitor?.Stop();
+#endif
+
+            if (UserPreferences.IsEnabled)
+            {
+#if UNITY_EDITOR_WIN
+                // Windows: Watch system theme changes in registry
+                registryMonitor = new RegistryMonitor(RegistryHive.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize");
+                registryMonitor.RegChanged += new EventHandler(OnRegChanged);
+                registryMonitor.Start();
+
+                // Set current system theme on start/when enabled
+                SetEditorThemeFromSystemTheme();
+#endif
+            }
+        }
+
+        private static void SetEditorThemeFromSystemTheme()
+        {
+#if UNITY_EDITOR_WIN
+            var appsUseLightTheme = registryMonitor.ReadDword("AppsUseLightTheme");
+
+            if (appsUseLightTheme == 1 && EditorGUIUtility.isProSkin)
+            {
+                Debug.Log("Editor: Switching to light theme....");
+
+                EditorPrefs.SetInt("UserSkin", 0);
+                InternalEditorUtility.SwitchSkinAndRepaintAllViews();
+            }
+            else if (appsUseLightTheme == 0 && !EditorGUIUtility.isProSkin)
+            {
+                Debug.Log("Editor: Switching to dark theme...");
+                EditorPrefs.SetInt("UserSkin", 1);
+                InternalEditorUtility.SwitchSkinAndRepaintAllViews();
+
+            }
+#endif
+        }
+    }
 }
 
---
+++ Editor/UserPreferences.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace AutoDarkTheme
+{
+	class UserPreferences : SettingsProvider
+	{
+		static readonly string KEY_EDITORPREF_IS_ENABLED = "AutoDarkTheme.IsEnabled";
+
+		public enum Mode
+        {
+			System,
+			Time
+        }
+
+		private static bool s_prefsLoaded = false;
+
+		private static bool s_isEnabled;
+		private static Mode s_mode;
+
+		internal class Styles
+		{
+			public static readonly GUIContent enabled = EditorGUIUtility.TrTextContent("Enabled");
+		}
+
+		public static event EventHandler PreferencesChanged;
+		private static void RaisePreferencesChanged()
+        {
+			var handler = PreferencesChanged;
+			handler?.Invoke(typeof(UserPreferences), EventArgs.Empty);
+        }
+
+		public static bool IsEnabled
+		{
+			get
+			{
+				LoadAllPreferenceValues();
+				return s_isEnabled;
+			}
+			set
+			{
+				s_isEnabled = value;
+				SaveAllPreferenceValues();
+			}
+		}
+
+		private static void LoadAllPreferenceValues()
+		{
+			if (!s_prefsLoaded)
+			{
+				s_isEnabled = EditorPrefs.GetBool(KEY_EDITORPREF_IS_ENABLED, true);
+
+				s_prefsLoaded = true;
+			}
+		}
+
+		private static void SaveAllPreferenceValues()
+		{
+			EditorPrefs.SetBool(KEY_EDITORPREF_IS_ENABLED, s_isEnabled);
+
+			RaisePreferencesChanged();
+		}
+
+		public UserPreferences(string path, SettingsScope scope = SettingsScope.User)
+			: base(path, scope)
+		{
+			LoadAllPreferenceValues();
+		}
+
+		public override void OnGUI(string searchContext)
+		{
+			s_isEnabled = EditorGUILayout.Toggle("Enabled", s_isEnabled);
+			EditorGUILayout.EnumPopup()
+
+			if (GUI.changed)
+			{
+				SaveAllPreferenceValues();
+			}
+		}
+
+		// Register the SettingsProvider
+		[SettingsProvider]
+		public static SettingsProvider CreateAssetGraphUserPreference()
+		{
+			var provider = new UserPreferences("Preferences/Auto Dark Theme")
+			{
+				keywords = GetSearchKeywordsFromGUIContentProperties<Styles>()
+			};
+
+			// Automatically extract all keywords from the Styles.
+			return provider;
+		}
+	}
+
+}
